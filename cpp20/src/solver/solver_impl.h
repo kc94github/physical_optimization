9df69a1f9063feb0c7cf372b32d3b5224cab8705
KailiangChen <KailiangChen@deeproute.ai>
2023-12-24 02:13:04 +0000

    Some progress

--- cpp20/src/solver/solver_impl.h
+++ cpp20/src/solver/solver_impl.h
@@ -5,6 +5,7 @@
 
 #include <OsqpEigen/OsqpEigen.h>
 #include <Eigen/Dense>
+#include <Eigen/Sparse>
 
 namespace Solver {
 
@@ -13,18 +14,16 @@ class SolverImpl {
     private:
 
         uint _size;
-        Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> _hessian_matrix;
-        Eigen::Vector<double, Eigen::Dynamic> _gradient_vector;
+        // Eigen::SparseMatrix<double> _hessian_matrix;
+        Eigen::SparseMatrix<double> _hessian_matrix;
+        
+        Eigen::VectorXd _gradient_vector;
 
-        Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> _equality_constraint_matrix;
-        Eigen::Vector<double, Eigen::Dynamic> _equality_constraint_vector;
+        // Eigen::Matrix<double, 0, Eigen::Dynamic> _total_constraint_matrix;
+        Eigen::SparseMatrix<double> _total_constraint_matrix;
+        Eigen::VectorXd _total_constraint_upper_bound;
+        Eigen::VectorXd _total_constraint_lower_bound;
 
-        Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> _inequality_constraint_matrix;
-        Eigen::Vector<double, Eigen::Dynamic> _inequality_constraint_vector;
-
-        // Use 0 size to distingulish with-bound and without-bound problem
-        Eigen::Vector<double, Eigen::Dynamic> _lower_bound_vector;
-        Eigen::Vector<double, Eigen::Dynamic> _upper_bound_vector;
 
         inline void reset_matrices() {
             // _hessian_matrix = Eigen::Matrix<double, _size, _size>::Zero();
@@ -36,14 +35,9 @@ class SolverImpl {
             _gradient_vector.resize(_size);
             _gradient_vector.setZero();
 
-            _equality_constraint_matrix.resize(0, _size);
-            _equality_constraint_vector.resize(0);
-
-            _inequality_constraint_matrix.resize(0, _size);
-            _inequality_constraint_vector.resize(0);
-
-            _lower_bound_vector.resize(0);
-            _upper_bound_vector.resize(0);
+            _total_constraint_matrix.resize(0, _size);
+            _total_constraint_upper_bound.resize(0);
+            _total_constraint_lower_bound.resize(0);
         }
 
     public:
@@ -74,12 +68,53 @@ class SolverImpl {
             return _gradient_vector;
         }
 
+        // inline auto equality_constraint_matrix() const {
+        //     return _equality_constraint_matrix;
+        // }
+
+        // inline auto equality_constraint_vector() const {
+        //     return _equality_constraint_vector;
+        // }
+
+        // inline auto inequality_constraint_matrix() const {
+        //     return _inequality_constraint_matrix;
+        // }
+
+        // inline auto inequality_constraint_vector() const {
+        //     return _inequality_constraint_vector;
+        // }
+
+        inline auto constraint_matrix() const {
+            return _total_constraint_matrix;
+        }
+
+        inline auto upper_bound_vector() const {
+            return _total_constraint_upper_bound;
+        }
+
+        inline auto lower_bound_vector() const {
+            return _total_constraint_lower_bound;
+        }
+
         bool add_value_to_gradient_vector(const uint row_index, const double value);
 
-        bool add_to_objective_function(const uint start_row, const uint start_col, const uint add_row_size, const uint add_col_size, const Eigen::MatrixXd& add_hessian_submatrix, const Eigen::VectorXd& add_gradient_subvector = Eigen::Vector<double, 0>::Zero());
+        bool add_to_objective_function(const uint start_row, const uint start_col, const uint add_row_size, const uint add_col_size, const Eigen::SparseMatrix<double>& add_hessian_submatrix, const Eigen::VectorXd& add_gradient_subvector = Eigen::Vector<double, 0>::Zero());
 
-        bool set_objective_function(const Eigen::MatrixXd& hessian_submatrix, const Eigen::VectorXd& gradient_subvector = Eigen::Vector<double, 0>::Zero());
+        bool set_objective_function(const Eigen::SparseMatrix<double>& hessian_submatrix, const Eigen::VectorXd& gradient_subvector = Eigen::Vector<double, 0>::Zero());
 
-};
+        bool set_upper_bound(const Eigen::VectorXd &upper_bound);
+
+        bool set_lower_bound(const Eigen::VectorXd &lower_bound);
+
+        bool add_equality_constraint(const Eigen::SparseMatrix<double> &equality_matrix, const Eigen::VectorXd &equality_vector);
+
+        bool add_inequality_constraint(const Eigen::SparseMatrix<double> &inequality_matrix, const Eigen::VectorXd &inequality_vector);
+
+        // bool _add_constraint_helper(const Eigen::SparseMatrix<double> &coefficient_matrix, const Eigen::VectorXd &constant_vector, Eigen::SparseMatrix<double> &constraint_matrix, Eigen::VectorXd &constraint_vector, uint size);
+
+        bool add_constraint_with_bounds(const Eigen::SparseMatrix<double> &constraint_matrix, const Eigen::VectorXd &upper_bound, const Eigen::VectorXd &lower_bound);
+
+        Eigen::Vector<double, Eigen::Dynamic> solve();
+};      
 
 }
